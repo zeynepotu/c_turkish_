afa80fefb197e09948cdcd33ca1541d8a8d32e8a
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-03-08 09:07:30 +0300

    Add setLocale to index.c

--- index.c
+++ index.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+int main()
+{
+    setlocale(LC_ALL,"Turkish"); // Dili türkçe olarak ayarladık. 
+    printf("MERHABA dünya"); // Artık Türkçe karakter içeren bir çıktı problemsiz şekilde çalışacak.
+    return 0;
+}
